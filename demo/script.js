0f1f80676eb9d1d5afe839881a5a901434aef552
Thomas Steiner <tomac@google.com>
2022-02-02 13:54:52 +0100

    Some fine-tuning

--- demo/script.js
+++ demo/script.js
@@ -3,13 +3,19 @@ import { potrace, init } from '../dist/index.js';
 (async () => {
   const pre1 = document.querySelector('#pre1');
   const pre2 = document.querySelector('#pre2');
+  const pre3 = document.querySelector('#pre3');
+  const pre4 = document.querySelector('#pre4');
   const div1 = document.querySelector('#div1');
   const div2 = document.querySelector('#div2');
+  const div3 = document.querySelector('#div3');
+  const div4 = document.querySelector('#div4');
 
-  const blob = await fetch('./snail.png').then((response) => response.blob());
+  const blob1 = await fetch('./logo.png').then((response) => response.blob());
+  const blob2 = await fetch('./avatar.jpg').then((response) => response.blob());
 
   await init();
-  potrace(blob, {
+
+  potrace(blob1, {
     turdsize: 2,
     turnpolicy: 4,
     alphamax: 1,
@@ -17,15 +23,15 @@ import { potrace, init } from '../dist/index.js';
     opttolerance: 0.2,
     pathonly: false,
     extractcolors: true,
-    posterizelevel: 5,
-    posterizationalgorithm: 1, // 0 - simple, 1 - interpolation
+    posterizelevel: 2,
+    posterizationalgorithm: 0, // 0: simple, 1: interpolation
   }).then((svg) => {
     div1.innerHTML = svg;
     svg = svg.replaceAll('><', '>\n<');
     pre1.textContent = svg + '\n\n(' + svg.length + ')';
   });
-  
-  potrace(blob, {
+
+  potrace(blob1, {
     turdsize: 2,
     turnpolicy: 4,
     alphamax: 1,
@@ -38,4 +44,35 @@ import { potrace, init } from '../dist/index.js';
     svg = svg.replaceAll('><', '>\n<');
     pre2.textContent = svg + '\n\n(' + svg.length + ')';
   });
+
+  potrace(blob2, {
+    turdsize: 2,
+    turnpolicy: 4,
+    alphamax: 1,
+    opticurve: 1,
+    opttolerance: 0.2,
+    pathonly: false,
+    extractcolors: true,
+    posterizelevel: 3,
+    posterizationalgorithm: 1, // 0: simple, 1: interpolation
+  }).then((svg) => {
+    div3.innerHTML = svg;
+    svg = svg.replaceAll('><', '>\n<');
+    pre3.textContent = svg + '\n\n(' + svg.length + ')';
+  });
+
+  potrace(blob2, {
+    turdsize: 2,
+    turnpolicy: 4,
+    alphamax: 1,
+    opticurve: 1,
+    opttolerance: 0.2,
+    pathonly: false,
+    extractcolors: false,
+  }).then((svg) => {
+    div4.innerHTML = svg;
+    svg = svg.replaceAll('><', '>\n<');
+    pre4.textContent = svg + '\n\n(' + svg.length + ')';
+  });
+
 })();
